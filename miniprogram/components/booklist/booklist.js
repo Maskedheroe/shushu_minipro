d7fc0936425f753f47ea1df58033feeb3fa6f222
方啸宇 <maskedfang@gmail.com>
2022-12-06 23:12:43 +0800

    完成列表开发

--- miniprogram/components/booklist/booklist.js
+++ miniprogram/components/booklist/booklist.js
@@ -4,7 +4,9 @@ Component({
    * 组件的属性列表
    */
   properties: {
-
+    bookitem: {
+      type: Object
+    }
   },
 
   /**
@@ -18,6 +20,6 @@ Component({
    * 组件的方法列表
    */
   methods: {
-
+    
   }
 })
